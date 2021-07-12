db47b063347be2a1e4844992686d55fff50b9202
ben <isieldor@gmail.com>
2021-07-12 17:03:37 -0500

    initial commit (client)

---
+++ client/src/App.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import L, { imageOverlay, LatLngExpression, Layer } from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+const Map = () => {
+  const defaultPosition: LatLngExpression = [48.864716, 2.349]; // Paris position
+  return (
+    <div className="map__container">
+      <MapContainer id="map" center={defaultPosition} zoom={13}>
+        <TileLayer url="hesparia.jpg"></TileLayer>
+      </MapContainer>
+    </div>
+  );
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Map></Map>
+    </div>
+  );
+}
+
+export default App;
