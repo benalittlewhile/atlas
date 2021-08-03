da4ec0d457043951f4ed5d5a2148962e586c9ff6
Snizzmo <63066191+Snizzmo@users.noreply.github.com>
2021-08-03 10:12:08 -0500

    adding/removing data

--- server/mongodb_test.py
+++ server/mongodb_test.py
@@ -3,9 +3,26 @@
 from pymongo import MongoClient
 # pprint library is used to make the output look more pretty
 from pprint import pprint
-# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+# connect to MongoDB
 client = MongoClient("mongodb+srv://admin:atlasadmin1@cluster0.cwe5c.mongodb.net/test")
-db=client.admin
+
+# set the db to be Hesparia 
+db=client['hesparia']
+
+# set the speciific collection 
+mycol=db['example1']
+# mycol.insert_one({'key1':'value1'})
+# mycol.delete_one({'key1':'value1'})
+
+pprint(mycol.estimated_document_count())
+
+if 'example1' in db.list_collection_names():
+    print('example1 exists')
 # Issue the serverStatus command and print the results
-serverStatusResult=db.command("serverStatus")
-pprint(serverStatusResult)
+#serverStatusResult=db.command("serverStatus")
+#pprint(serverStatusResult)
+
+# Get some data output from hesparia db
+# pprint(client.hesparia.objects)
+
+
